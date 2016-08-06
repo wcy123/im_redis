7dd7792f22141da7a3bb3f72f002e270a5265ee9
Wang Chunye <wcy123@gmail.com>
2016-08-07 02:07:55 +0800

    fix compilation error

--- src/im_redis.erl
+++ src/im_redis.erl
@@ -1,7 +1,7 @@
 -module(im_redis).
 -author('wcy123@gmail.com').
 -compile([{parse_transform, lager_transform}]).
--export([q/2,pq/2]).
+-export([q/2,qp/2]).
 
 
 is_block() ->
@@ -10,10 +10,13 @@ timeout() ->
     infinity.
 
 q(Name, Q) ->
-    request(Name, fun eredis:q/2, [Q]).
-pq(Name, PQ) ->
-    request(Name, fun eredis:pq/2, [PQ]).
+    request(Name, fun my_q/2, [Q]).
+qp(Name, QP) ->
+    request(Name, fun my_qp/2, [QP]).
 
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 request(Name, Fun, Args) ->
     try
         Worker = poolboy:checkout(Name, is_block(), timeout()),
@@ -38,3 +41,56 @@ do_work(Name, Pid, Fun, Args) ->
     after
         ok = poolboy:checkin(Name, Pid)
     end.
+
+my_q(Client, Q) ->
+    try eredis:q(Client, Q) of
+        {error, no_connection} ->
+            lager:error("redis op: input = ~p, error reason:~p", [Q, no_connection]),
+            {error, no_connection};
+        {error, Reason} ->
+            lager:error("redis op: input = ~p, error reason:~p", [Q, Reason]),
+            {error, Reason};
+        {ok, Value} ->
+            lager:debug("redis op: ~p => ~p~n", [Q, Value]),
+            {ok, Value}
+    catch
+        Class:Exception ->
+            lager:debug("redis op ~p:~p input = ~w~n       stack = ~p~n",
+                        [Class, Exception, erlang:get_stacktrace()]),
+            {error, {Class, Exception}}
+    end.
+
+my_qp(Client, QP) ->
+    try eredis:qp(Client, QP) of
+        RetList when is_list(RetList) ->
+            qp_check(QP, RetList),
+            RetList;
+        {error, no_connection} ->
+            lager:error("redis op: input = ~w, error reason:~w", [QP, no_connection]),
+            {error, no_connection};
+        {error, Reason} ->
+            lager:error("redis op: input = ~w, error reason:~w", [QP, Reason]),
+            {error, Reason}
+    catch
+        Class:Exception ->
+            lager:error("redis op ~p:~p input = ~w~n       stack = ~p~n",
+                        [Class, Exception, erlang:get_stacktrace()]),
+            {error, {Class, Exception}}
+    end.
+
+qp_check(QP, RetList) ->
+    lists:foreach(
+      fun({Q, Ret}) ->
+              case Ret of
+                  {error, no_connection} ->
+                      lager:error("error reason:no_connection, input = ~p~n", [Q]),
+                      {error, no_connection};
+                  {error, Reason} ->
+                      lager:error("error reason:~p, Q = ~p~n", [Q, Reason]),
+                      {error, Reason};
+                  {ok, Value} ->
+                      lager:debug("redis op: ~p => ~p~n", [Q, Value]),
+                      {ok, Value}
+              end
+      end,
+      lists:zip(QP, RetList)).
